659fef2249360efa686f8d109a51cf516bc32ba3
Hridik Dev <hridikori@gmail.com>
2025-09-08 16:20:18 +0530

    initial commit

---
+++ app/src/main/java/com/example/torchblink/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.torchblink;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraAccessException;
+import android.widget.Toast;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    private  CameraManager cameraManager;
+    private String CameraId;
+
+    private Handler handler=new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btn=findViewById(R.id.button);
+
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            if(cameraManager!=null){
+                CameraId = cameraManager.getCameraIdList()[0];
+            }
+        }catch (CameraAccessException e){
+            Toast toast = null;
+            toast.makeText((Context) MainActivity.this, (CharSequence) e,Toast.LENGTH_SHORT).show();
+        }
+        btn.setOnClickListener(v->{blinkTorch(10);});
+    }
+
+    private void blinkTorch(int times) {
+        final int delay = 500;
+        for(int i=0;i<times;i++){
+            int finalI = i;
+            handler.postDelayed(()->{
+                try {
+                    Boolean state= (finalI%2==0);
+                    cameraManager.setTorchMode(CameraId,state);
+                }catch (CameraAccessException e){
+                    Toast toast = null;
+                    toast.makeText((Context) MainActivity.this, (CharSequence) e,Toast.LENGTH_SHORT).show();
+                }
+            },i*delay);
+        }
+    }
+
+
+}
